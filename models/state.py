5fe3bfa3df033d3720dc79140ea86fc457680248
Your Name <you@example.com>
2024-02-19 23:49:22 +0200

    test

--- models/state.py
+++ models/state.py
@@ -13,16 +13,17 @@ from sqlalchemy.orm import relationship
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
-
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade="all, delete-orphan")
-
-@property
-def cities(self):
-    """Getter attribute to retrieve cities associated with this state"""
-    cities = []
-    citiesList = storage.all("City")  # Retrieve all City instances
-    for city in citiesList.values():
-        if city.state_id == self.id:
-            cities.append(city)
-    return cities
+    
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state", cascade="delete")
+    else:
+        @property
+        def cities(self):
+            """Getter attribute to retrieve cities associated with this state"""
+            cities = []
+            citiesList = storage.all("City")  # Retrieve all City instances
+            for city in citiesList.values():
+                if city.state_id == self.id:
+                    cities.append(city)
+            return cities
