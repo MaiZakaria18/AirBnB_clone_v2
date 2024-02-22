8d9e6886b0761091b007a70e34313cc21dd94ea9
Ahmednabil14 <ahmednabil14499@gmail.com>
2024-02-23 01:13:41 +0200

    fix task 2

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -66,7 +66,6 @@ class FileStorage:
     Returns:
         None
         """
-        def delete(self, obj=None):
-            """Delete a given object from __objects, if it exists"""
-            if obj:
-                del self.__objects["{}.{}".format(type(obj).__name__, obj.id)]
+        if obj:
+            obj = obj.to_dict()['__class__'] + '.' + obj.to_dict()['id']
+            del self.__objects[obj]
