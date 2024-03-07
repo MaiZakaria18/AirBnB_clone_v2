c2c42805e4c73f02d805f928f201a17ef2e8e214
Ahmednabil14 <ahmednabil14499@gmail.com>
2024-03-07 19:02:21 +0200

    task 0

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+# Bash script that sets up your web servers for the deployment of web_static
+sudo apt-get update
+sudo apt-get install -y nginx
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "Helle from Ngnix" > /data/web_static/releases/test/index.html
+rm -f /data/web_static/current/
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+# -R to change owner to the directory and all files
+# user:group
+sudo chown -R ubuntu:ubuntu /data/
+echo "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    root /var/www/html;
+    server_name _;
+
+    location / {
+        index index.html index.nginx-debian.html;
+        }
+    location /hbnb_static {
+        alias /data/web_static/current/;
+    }
+} " > /etc/nginx/sites-available/default
+sudo service nginx restart
