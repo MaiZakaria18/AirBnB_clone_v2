7de0941f71658d015a2c08f38b51a50b270f22af
Your Name <you@example.com>
2024-03-28 00:09:24 +0200

    task8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -8,19 +8,21 @@ app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """Displays an HTML page with a list of all State objects in DBStorage.
+
+    States are sorted by name.
+    """
+    states = storage.all("State")
+    return render_template("7-states_list.html", states=states)
+
+
 @app.teardown_appcontext
-def dispose(exception):
-    """ Remove current session """
+def teardown(exc):
+    """Remove the current SQLAlchemy session."""
     storage.close()
 
 
-@app.route('/states_list')
-def states():
-    """ Display list of all the states """
-    states = storage.all(State)
-    states_list = list(states.values())
-    return render_template('7-states_list.html', states=states_list)
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
